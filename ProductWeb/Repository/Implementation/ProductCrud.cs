59de13db15c9663c6e5a0b43c9d069664ca66bf3
AABurmov18 <AABurmov18@codingburgas.bg>
2023-03-14 10:46:20 +0200

    Implement GetAll products method

---
+++ ProductWeb/Repository/Implementation/ProductCrud.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductWeb.Models.Domain;
+using ProductWeb.Models.DTO;
+using ProductWeb.Repository.Interface;
+
+namespace ProductWeb.Repository.Implementation
+{
+    public class ProductCrud : IProductCrud
+    {
+        private readonly DatabaseContext _databaseContext;
+
+        public ProductCrud(DatabaseContext databaseContext)
+        {
+            this._databaseContext = databaseContext;
+        }
+
+        public virtual async Task<ICollection<Product>> GetAll()
+        {
+            return await _databaseContext.products.ToListAsync();
+        }
+    }
+}
---
+++ ProductWeb/Repository/Interface/IProductCrud.cs
@@ -0,0 +1,9 @@
+﻿using ProductWeb.Models.DTO;
+
+namespace ProductWeb.Repository.Interface
+{
+    public interface IProductCrud
+    {
+        Task<ICollection<Product>> GetAll();
+    }
+}
