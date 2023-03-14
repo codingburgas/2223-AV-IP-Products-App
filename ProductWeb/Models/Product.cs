2936a961fff3d2c4e9f9bf0edd1c756ec5296089
Kristian Milanov <kamilanov18@codingburgas.bg>
2023-03-14 08:49:06 +0200

    Add EFC dependecies and Product model

---
+++ ProductWeb/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductWeb.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MinLength(3)]
+        [MaxLength(50)]
+        public string Name { get; set; }
+        [MaxLength(200)]
+        [Required]
+        public string Description { get; set; }
+        [Range(1,200)]
+        [Required]
+        public int Price { get; set; }
+        [Required]
+        public DateTime DateOfCreation { get; set; }
+        [Required]
+        public DateTime DateOfLastCahnge { get; set; }
+    }
+}
