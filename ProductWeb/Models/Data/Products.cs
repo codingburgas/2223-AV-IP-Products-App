fd95a2c88c358f0a820a0fb13a8841107c8dd465
ttangelov18 <ttangelov18@codingburgas.bg>
2023-03-14 14:20:50 +0200

    did some efcore stuff

---
+++ ProductWeb/Models/Data/Products.cs
@@ -0,0 +1,14 @@
+﻿#pragma warning disable 8618
+
+namespace ProductWeb.Models.Data
+{
+    public class Products
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastUpdated { get; set; }
+    }
+}
