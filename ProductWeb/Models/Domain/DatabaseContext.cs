5c715b1b70d58def4d97cef7feced60e456bedaf
AABurmov18 <AABurmov18@codingburgas.bg>
2023-03-14 09:42:31 +0200

    Add dbcontext class and configure db settings

---
+++ ProductWeb/Models/Domain/DatabaseContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProductWeb.Models.Domain
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options):base(options)
+        {
+            
+        }
+    }
+}
--- ProductWeb/Program.cs
+++ ProductWeb/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using ProductWeb.Models.Domain;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<DatabaseContext>
+    (options => options.UseSqlServer(builder.Configuration.GetConnectionString("conn")));
 
 var app = builder.Build();
 
