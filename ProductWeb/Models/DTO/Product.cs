19295320a4f32d88390fff56fa0471b8076f1041
AABurmov18 <AABurmov18@codingburgas.bg>
2023-03-14 09:58:45 +0200

    Create Product class

--- ProductWeb/Models/DTO/Product.cs
+++ ProductWeb/Models/DTO/Product.cs
@@ -1,6 +1,29 @@
-﻿namespace ProductWeb.Models.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductWeb.Models.DTO
 {
     public class Product
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(50)]
+        [RegularExpression("^[a-zA-Z ]*$")]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Description { get; set; }
+
+        [Required]
+        [MinLength(1)]
+        [MaxLength(200)]
+        public byte Price { get; set; }
+
+        public DateOnly createdOn { get; set; }
+
+        public DateOnly updatedOn { get; set; }
     }
 }
