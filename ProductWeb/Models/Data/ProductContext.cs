4ccad568a731c5ac3f8aaf726f0c2474b063a12d
ttangelov18 <ttangelov18@codingburgas.bg>
2023-03-15 09:22:15 +0200

    code-first database created

---
+++ ProductWeb/Models/Data/ProductContext.cs
@@ -0,0 +1,19 @@
+﻿#pragma warning disable 8618
+
+using Microsoft.EntityFrameworkCore;
+
+namespace ProductWeb.Models.Data
+{
+    public class ProductContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=ProductsDb");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<Products>().Property(x => x.Id).IsRequired();
+        }
+        public DbSet<Products> Products { get; set; }
+    }
+}
