7d63cce4b6933a06db8f6f156524556bf7eb406c
TATinev18 <TATinev18@codingburgas.bg>
2023-03-15 09:16:29 +0200

    Implement Products Model

---
+++ ProductsProject/Models/Products.cs
@@ -0,0 +1,13 @@
+﻿namespace ProductsProject.Models
+{
+    public class Products
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;   
+        public DateTime UpdatedDate { get; set; } = DateTime.Now;
+
+    }
+}
