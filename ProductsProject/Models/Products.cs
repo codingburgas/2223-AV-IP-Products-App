003d37801009423b2418ca629d6dcff6565f089b
TATinev18 <TATinev18@codingburgas.bg>
2023-03-15 09:23:15 +0200

    Add Data Annotations

--- ProductsProject/Models/Products.cs
+++ ProductsProject/Models/Products.cs
@@ -1,10 +1,16 @@
-﻿namespace ProductsProject.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductsProject.Models
 {
     public class Products
     {
-        public int Id { get; set; } 
+        [Key]
+        public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Description { get; set; }
+        [Required]
         public decimal Price { get; set; }
         public DateTime CreatedDate { get; set; } = DateTime.Now;   
         public DateTime UpdatedDate { get; set; } = DateTime.Now;
