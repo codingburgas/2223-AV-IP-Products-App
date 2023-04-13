010b8c2c75666e936d8e0114214d4fc71697b66a
IDIliev18 <IDIliev18@codingburgas.bg>
2023-04-13 09:41:14 +0300

    Implement text to input box on click

--- ProductWeb/wwwroot/js/site.js
+++ ProductWeb/wwwroot/js/site.js
@@ -2,3 +2,17 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$("#editButton").click(function () {
+
+    $("#productName").html(`<label asp-for="Name" class="control-label"></label>
+                            <input asp-for="Name" class="form-control" />
+                            <span asp-validation-for="Name" class="text-danger"></span>`)
+
+    $("#productDescription").html(`<label asp-for="Description" class="control-label"></label>
+                                   <input asp-for="Description" class="form-control" />
+                                   <span asp-validation-for="Name" class="text-danger"></span>`)
+
+    $("#productPrice").html(`<label asp-for="Price" class="control-label"></label>
+                             <input asp-for="Price" class="form-control" />
+                             <span asp-validation-for="Price" class="text-danger"></span>`)
+})
